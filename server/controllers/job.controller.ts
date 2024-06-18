1381749d71d360b9cc205f5b9711414c5a252679
namanjangid2002 <namanjangid2002@gmail.com>
2024-06-18 19:37:15 +0530

    getAllJobs & getJob added

--- server/controllers/job.controller.ts
+++ server/controllers/job.controller.ts
@@ -68,3 +68,64 @@ export const deleteJob = async (req: Request, res: Response, next: NextFunction)
         next(error);
     }
 }
+
+
+export const getAllJobs = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authorId = req.query.authorId as string;
+        const userId = req.query.userId as string;
+
+        const jobs = await prisma.job.findMany({
+            where: {
+                OR: [
+                    {
+                        authorId: authorId
+                    },
+                    {
+                        editorId: userId
+                    }
+                ],
+            },
+            include: {
+                author: true,
+                editor: true,
+            },
+        });
+        res.status(200).json(jobs);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getJob = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authorId = req.query.authorId as string;
+        const editorId = req.query.editorId as string;
+        const jobId = req.query.jobId as string;
+
+        if (!jobId || (!authorId && !editorId)) {
+            return res.status(400).json({ error: "jobId and either authorId or editorId must be provided" });
+
+        }
+
+        const job = await prisma.job.findFirst({
+            where: {
+                id: jobId,
+                authorId: authorId,
+                editorId: editorId
+            },
+            include: {
+                author: true,
+                editor: true,
+            },
+        });
+        if (!job) {
+            return res.status(404).send("Job not found.");
+        }
+
+        res.status(200).json(job);
+
+    } catch (error) {
+        next(error);
+    }
+}
--- server/routes/job.routes.ts
+++ server/routes/job.routes.ts
@@ -1,13 +1,15 @@
 import { Router, type NextFunction, type Request, type Response } from "express";
-import { createJob, deleteJob, readJob, updateJob } from "../controllers/job.controller.js";
+import { createJob, deleteJob, getAllJobs, getJob, readJob, updateJob } from "../controllers/job.controller.js";
 import { validateEditorRole } from "../middlewares/validEditor.js";
 
 const router = Router();
 
-type RequestHandler = (req: Request, res: Response,next:NextFunction) => void;
+type RequestHandler = (req: Request, res: Response, next: NextFunction) => void;
 
 router.post("/createJob", validateEditorRole as RequestHandler, createJob as RequestHandler);
 router.get("/readJob/:id", readJob as RequestHandler);
 router.put("/updateJob/:id", validateEditorRole as RequestHandler, updateJob as RequestHandler);
 router.delete("/deleteJob/:id", deleteJob as RequestHandler);
+router.get("/allJobs", getAllJobs as RequestHandler);
+router.get("/getJob", getJob as RequestHandler);
 export default router;
